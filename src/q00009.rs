e8159781b63dd7057cf0e6ad8a4900d7bdf66de6
Daniel Lu <daniel@lawrence.lu>
2023-04-02 19:35:07 -0700

    add 9

---
+++ src/q00009.rs
@@ -0,0 +1,13 @@
+pub fn is_palindrome(x: i32) -> bool {
+    let s = x.to_string();
+    return s == s.chars().rev().collect::<String>();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn q00009() {
+        assert!(super::is_palindrome(12321));
+        assert!(!super::is_palindrome(12345));
+    }
+}
